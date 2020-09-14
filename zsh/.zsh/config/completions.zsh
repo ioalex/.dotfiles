ed84509edc7593cd448443cbe3bc2056bb0ebb99
user.email <alex@alexhe.io>
2020-09-14 16:00:12 +1000

    Use Stow --dotfiles feature, shell config changes

--- zsh/.zsh/config/completions.zsh
+++ zsh/.zsh/config/completions.zsh
@@ -1,14 +1,32 @@
 # Autocompletion
 # Enable Autocompletion
 autoload -Uz compinit;
+zstyle ':completion::complete:*' cache-path $ZSH_COMPLETE
+compinit -d $ZSH_DUMP
 
 # Speed up zsh compinit by only checking cache once a day
-typeset -i updated_at=$(date +"%j" -r ~/.zcompdump 2>/dev/null || stat -f "%Sm" -t "%j" ~/.zcompdump 2>/dev/null)
+typeset -i updated_at=$(date +"%j" -r $ZSH_COMPLETE 2>/dev/null || stat -f "%Sm" -t "%j" $ZSH_COMPLETE 2>/dev/null)
 if [ $(date +"%j") != $updated_at ]; then
   compinit -i
 else
   compinit -C -i
 fi
+
+# _zpcompinit_custom() {
+#   setopt extendedglob local_options
+#   autoload -Uz compinit
+#   local zcd=$ZSH_DUMP/.zcompdump
+#   local zcdc="$zcd.zwc"
+#   # Compile the completion dump to increase startup speed, if dump is newer or doesn't exist,
+#   # in the background as this is doesn't affect the current session
+#   if [[ -f "$zcd"(#qN.m+1) ]]; then
+#         compinit -i -d "$zcd"
+#         { rm -f "$zcdc" && zcompile "$zcd" } &!
+#   else
+#         compinit -C -d "$zcd"
+#         { [[ ! -f "$zcdc" || "$zcd" -nt "$zcdc" ]] && rm -f "$zcdc" && zcompile "$zcd" } &!
+#   fi
+# }
 
 # Load complist module
 # Provides menu list for select completion results
--- zsh/.zsh/config/config.zsh
+++ zsh/.zsh/config/config.zsh
@@ -4,9 +4,3 @@ setopt autocd		# Automatically cd into typed directory.
 
 # Enable 256 color to make auto-suggestions look nice
 export TERM="xterm-256color"
-
-# Set ZSH_CACHE_DIR to the path where cache files should be created
-# This is needed to ensure the zsh_reload plugin works properly <github.com/ohmyzsh/ohmyzsh/blob/master/plugins/zsh_reload/zsh_reload.plugin.zsh>
-if [[ -z "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="$ZSH/cache"
-fi
