453057fca08305d252b4e9326375bf631e26b8c0
user.email <alex@alexhe.io>
2020-09-16 14:19:26 +1000

    Test bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+# Check if the unzip command is available
+if [ ! "$(command -v unzip)" ]; then
+  echo "The unzip command was not found. Please install it and try again." >&2
+  exit 1
+fi
+
+# Fetch dotfiles
+fetchSource(){
+  wget -O /tmp/dotfiles.zip https://github.com/ioalex/dotfiles/archive/master.zip
+  mkdir -p ~/Desktop/dotfiles
+
+  cd ~/Desktop/dotfiles || exit
+  unzip -o "/tmp/dotfiles.zip"
+  mv dotfiles-master/* ./
+  rm -rf dotfiles-master
+  rm /tmp/dotfiles.zip
+}
