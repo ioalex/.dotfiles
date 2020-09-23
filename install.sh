ed1ac2fd9f4f8cd8ddf9f72af64f11d467ff2050
user.email <alex@alexhe.io>
2020-09-24 04:00:50 +1000

    Install script changes - split into two

--- install.sh
+++ install.sh
@@ -9,52 +9,19 @@
 
 # Installs .dotfiles
 
-Check for unzip before we continue
+# Check for unzip before we continue
 if ! command -v unzip >/dev/null 2>&1; then
-  echo '\u274c: The 'unzip' command is required but was not found. Please install unzip first and then run this script again.' >&2
+  echo "\u274c: The 'unzip' command is required but was not found. Please install unzip first and then run this script again." >&2
   exit 1
 fi
 
 fetchSources(){
   wget -O /tmp/dotfiles.zip https://github.com/ioalex/.dotfiles/archive/master.zip
-  mkdir -p ~/Desktop/.dotfiles/
+  # mkdir -p ~/Desktop/.dotfiles/
 
-  cd ~/Desktop/.dotfiles/ || { echo "\u274c: Failure: ~/.dotfiles/ not found!"; exit 1; }
-  unzip -o "/tmp/dotfiles.zip"
+  cd ~/Desktop/ || { echo "\u274c: Failure: ~/.dotfiles/ not found!"; exit 1; }
+  unzip -o "/tmp/dotfiles.zip" -d ~/Desktop/.dotfiles
   rm /tmp/dotfiles.zip
 }
 
 fetchSources;
-
-echo "Welcome to the Dotfiles Wizard!"
-echo "This is a script that will attempt to install $(whoami)'s dotfiles"
-echo "\n"
-
-
-echo "Executing Homebrew script..."
-. brew/brew.sh
-echo "\n"
-
-echo "Executing ZSH script..."
-. home/.config/zsh/zsh.sh
-echo "\n"
-
-echo "Executing Alacritty script..."
-. home/.config/alacritty/alacritty.sh
-echo "\n"
-
-echo "Executing Tmux script..."
-. home/.config/tmux/tmux.sh
-echo "\n"
-
-echo "Executing Ranger script..."
-. home/.config/ranger/ranger.sh
-echo "\n"
-
-echo "Executing Nano script..."
-. home/.config/nano/nano.sh
-echo "\n"
-
-echo "Executing Micro script..."
-. home/.config/micro/micro.sh
-echo "\n"
---
+++ scripts/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# install.sh
+# Author: Alex <alex@alexhe.io>
+# Year: 2020
+#
+# Distributed under terms of the Unlicense license.
+#
+
+# .dotfiles Setup
+
+echo "\n"
+echo "Welcome to the Dotfiles Setup Wizard!"
+echo "This is a script that will attempt to install $(whoami)'s dotfiles"
+echo "\n"
+
+
+echo "Executing Homebrew script..."
+. brew/brew.sh
+echo "\n"
+
+echo "Executing ZSH script..."
+. home/.config/zsh/zsh.sh
+echo "\n"
+
+# Add a script that creates directories needed to symlink
+# Add a script that moves old configuration that will be replaced or is uneeded into a 'Recovered' folder
+
+echo "Executing GNU Stow Script"
+. scripts/stow.sh
+echo "\n"
+
+echo "Executing Alacritty script..."
+. home/.config/alacritty/alacritty.sh
+echo "\n"
+
+echo "Executing Tmux script..."
+. home/.config/tmux/tmux.sh
+echo "\n"
+
+echo "Executing Ranger script..."
+. home/.config/ranger/ranger.sh
+echo "\n"
+
+echo "Executing Nano script..."
+. home/.config/nano/nano.sh
+echo "\n"
+
+echo "Executing Micro script..."
+. home/.config/micro/micro.sh
+echo "\n"
