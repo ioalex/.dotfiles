4fefc47fa23117a59cb7d02e033bd62b782b768c
user.email <alex@alexhe.io>
2020-09-23 20:33:53 +1000

    Added tmux and ranger script, tpm folder changes

---
+++ home/.config/ranger/ranger.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Installs Ranger Plugins
+git clone https://github.com/alexanderjeurissen/ranger_devicons "$XDG_CONFIG_HOME/ranger/plugins/ranger_devicons"
+echo "Plugins for Ranger have been installed."
---
+++ home/.config/tmux/tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Installs Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm "$XDG_CONFIG_HOME/tmux/plugins/tpm"
+echo "Tmux Plugin Manager has been installed."
